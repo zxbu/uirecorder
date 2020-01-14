0cc7db37c004fd401aad83c7ca8e1c0c0aa33a85
周鑫 <zhouxin@yijiupi.cn>
2020-01-14 17:25:03 +0800

    修改回放脚本

--- project/run.sh
+++ project/run.sh
@@ -1,5 +1,8 @@
 if [ "$1" = "" ]; then
     npm run paralleltest
 else
-    npm run singletest $1 $2
+    recodId=`head -1 $1 | awk '{print $2}'`
+    curl -d $recodId $UIRECORDER_MOCK_API/uirecorder-mock-manager/beginReplay
+    npm run singletest $1
+    curl -d $recodId $UIRECORDER_MOCK_API/uirecorder-mock-manager/endReplay
 fi
